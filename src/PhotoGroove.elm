2a5f6c11931a441d2d7e317c6c0b58cb37c532df
Filip Mihalic <filip.mihalic@yahoo.com>
2020-11-14 23:09:58 +0100

    Initial commit

---
+++ src/PhotoGroove.elm
@@ -0,0 +1,118 @@
+module PhotoGroove exposing (main)
+
+import Array exposing (Array)
+import Browser exposing (..)
+import Html exposing (Html, button, div, h1, h3, img, input, label, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+type alias Msg =
+    { description : String, data : String }
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , button [ onClick { description = "ClickedSurpriseMe", data = "" } ] [ text "Surprise me!" ]
+        , h3 [] [ text "Thumbnail Size:" ]
+        , div [ id "choose-size" ] (List.map viewSizeChooser [ Small, Medium, Large ])
+        , div [ id "thumbnails", class (sizeToString model.chosenSize) ] (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ]
+
+
+viewThumbnail : String -> Photo -> Html Msg
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "Clicked photo", data = thumb.url }
+        ]
+        []
+
+
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size" ]
+            [ text (sizeToString size) ]
+        ]
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    , chosenSize : ThumbnailSize
+    }
+
+
+initialModel : Model
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    , chosenSize =
+        Medium
+    }
+
+
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+update msg model =
+    case msg.description of
+        "ClickedPhoto" ->
+            { model | selectedUrl = msg.data }
+
+        "ClickedSurpriseMe" ->
+            { model | selectedUrl = "2.jpeg" }
+
+        _ ->
+            model
+
+
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
